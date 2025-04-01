b51680493b46954e736bdb8b86d0f99028146b1e
Kuwaann <emirrivaldy@gmail.com>
2025-04-01 20:13:17 +0700

    Upload Laravel Project

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Posts', 'posts' => [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Emir Rivaldy',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Soluta at nam quisquam necessitatibus dolorem incidunt, 
+                doloribus numquam libero sint quibusdam tempora aliquam 
+                officia perspiciatis, esse obcaecati vel nostrum, eveniet neque?'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Muhammad Emir Rivaldys',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, enim sapiente, hic veniam sequi eos nemo repellendus, nesciunt vel in pariatur voluptatem veritatis animi explicabo atque ipsam reprehenderit! Dolorum, maxime!'
+        ]
+
+    ]]);
+});
+
+Route::get('/posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Muhammad Emir Rivaldy',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Soluta at nam quisquam necessitatibus dolorem incidunt, 
+                doloribus numquam libero sint quibusdam tempora aliquam 
+                officia perspiciatis, esse obcaecati vel nostrum, eveniet neque?'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Muhammad Emir Rivaldys',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, enim sapiente, hic veniam sequi eos nemo repellendus, nesciunt vel in pariatur voluptatem veritatis animi explicabo atque ipsam reprehenderit! Dolorum, maxime!'
+        ]
+
+        ];
+
+        $post = Arr::first($posts, function($post){
+            return $post['id'];
+        });
+        
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contacts', function () {
+    return view('contacts', ['title' => 'Contacts']);
+});
